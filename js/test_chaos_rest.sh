d1a00fdeba6b583226a3ee5da6a4e9fdbccdcc42
amichelo <andrea.michelotti@lnf.infn.it>
2017-12-01 00:07:20 +0100

    config

--- js/test_chaos_rest.sh
+++ js/test_chaos_rest.sh
@@ -16,6 +16,8 @@ if ! check_proc mds ;then
     nok_mesg "mds running"
     end_test 1 "mds dead"
 fi
+echo "1512080677 1277.13 836.85 0.0 84800.0 1 106 FFFFFFFF FFFFFFFF FFFFFFFF FFC00000 106 FFFFFFFF FFFFFFFF FFFFFFFF FFC00000 3 2 2 210258 0 -1 5.33e+00 1.67e+02 0 368667000 0.000 0.000 5201.4 0.0 1 0 0.000 0.000 0.000 0.000 -3.550 -0.370 -2.940 -0.510 -1.720 -0.470 1.360 0.490 20.33 1770.00 18.90 20.89 1.842 1.782 -0.745 1.520 " > newdafne.stat
+
 if launch_us_cu 1 100 $CHAOS_MDS $USNAME TEST 1;then
 	if ! check_proc $USNAME;then
 	    error_mesg "$USNAME quitted"
