e6b677f0d8126c6f5a61338dfe776eb8d024e7b1
amichelo <andrea.michelotti@lnf.infn.it>
2018-01-25 17:42:35 +0100

    added transition tests

--- js/test/test-transitions.js
+++ js/test/test-transitions.js
@@ -55,52 +55,67 @@ describe('CHAOS TEST TRANSITIONS', function () {
 			cu_status = ll;
 			if (cu_status.length == 0)
 				done();
-			jchaos.sendCUCmd(cu_status, "deinit", "", null);
-			jchaos.checkLive(cu_status, 10, 1000, function (ds) { return (ds.health.nh_status == "Deinit"); }, function () { done(0); }, function () { done(1) });
+//		    jchaos.sendCUCmd(cu_status, "deinit", "", null);
+		    jchaos.node(cu_status, "deinit", "cu", null, null);
+			jchaos.checkLive(cu_status, 10, 5000, function (ds) { return (ds.health.nh_status == "Deinit"); }, function () { done(0); }, function () { done(1) });
 
 		});
 	});
-	it('check Deinit->Unload', function (done) {
+	// it('check Deinit->Unload', function (done) {
+	// 	var cu_status = [];
+	// 	jchaos.getCUStatus("Deinit", function (ll) {
+	// 		console.log("N. CU in Deinit:" + ll.length);
+	// 		cu_status = ll;
+	// 		if (cu_status.length == 0)
+	// 			done();
+	// 		cu_status.forEach(function (elem) {
+	// 			jchaos.loadUnload(elem, false, null);
+	// 		});
+	// 		jchaos.checkLive(cu_status, 10, 5000, function (ds) { return (ds.health.nh_status == "Unload"); }, function () { done(0); }, function () { done(1) });
+
+	// 	});
+	// });
+	// it('check Unload->Load', function (done) {
+	// 	var cu_status = [];
+	// 	jchaos.getCUStatus("Unload", function (ll) {
+	// 		console.log("N. CU in Unload:" + ll.length);
+	// 		cu_status = ll;
+	// 		if (cu_status.length == 0)
+	// 			done();
+	// 		cu_status.forEach(function (elem) {
+	// 			jchaos.loadUnload(elem, true, null);
+	// 		});
+	// 		jchaos.checkLive(cu_status, 10, 5000, function (ds) { return (ds.health.nh_status == "Load"); }, function () { done(0); }, function () { done(1) });
+
+	// 	});
+	// });
+	// it('check Load->Init', function (done) {
+	// 	var cu_status = [];
+	// 	jchaos.getCUStatus("Load", function (ll) {
+	// 		console.log("N. CU in Load:" + ll.length);
+	// 		cu_status = ll;
+	// 		if (cu_status.length == 0)
+	// 			done();
+	// 		jchaos.sendCUCmd(cu_status, "init", "", null);
+
+	// 		jchaos.checkLive(cu_status, 10, 1000, function (ds) { return (ds.health.nh_status == "Init"); }, function () { done(0); }, function () { done(1) });
+
+	// 	});
+    // });
+	it('check Deinit->Init', function (done) {
 		var cu_status = [];
 		jchaos.getCUStatus("Deinit", function (ll) {
 			console.log("N. CU in Deinit:" + ll.length);
 			cu_status = ll;
 			if (cu_status.length == 0)
 				done();
-			cu_status.forEach(function (elem) {
-				jchaos.loadUnload(elem, false, null);
-			});
-			jchaos.checkLive(cu_status, 10, 1000, function (ds) { return (ds.health.nh_status == "Unload"); }, function () { done(0); }, function () { done(1) });
+//		    jchaos.sendCUCmd(cu_status, "deinit", "", null);
+		    jchaos.node(cu_status, "init", "cu", null, null);
+		    jchaos.checkLive(cu_status, 10, 5000, function (ds) { return (ds.health.nh_status == "Init"); }, function () { done(0); }, function () { done(1) });
 
 		});
 	});
-	it('check Unload->Load', function (done) {
-		var cu_status = [];
-		jchaos.getCUStatus("Unload", function (ll) {
-			console.log("N. CU in Unload:" + ll.length);
-			cu_status = ll;
-			if (cu_status.length == 0)
-				done();
-			cu_status.forEach(function (elem) {
-				jchaos.loadUnload(elem, true, null);
-			});
-			jchaos.checkLive(cu_status, 10, 1000, function (ds) { return (ds.health.nh_status == "Load"); }, function () { done(0); }, function () { done(1) });
-
-		});
-	});
-	it('check Load->Init', function (done) {
-		var cu_status = [];
-		jchaos.getCUStatus("Load", function (ll) {
-			console.log("N. CU in Load:" + ll.length);
-			cu_status = ll;
-			if (cu_status.length == 0)
-				done();
-			jchaos.sendCUCmd(cu_status, "init", "", null);
-
-			jchaos.checkLive(cu_status, 10, 1000, function (ds) { return (ds.health.nh_status == "Init"); }, function () { done(0); }, function () { done(1) });
-
-		});
-	});
+    
 	it('check Init->Start', function (done) {
 		var cu_status = [];
 		jchaos.getCUStatus("Init", function (ll) {
@@ -110,7 +125,7 @@ describe('CHAOS TEST TRANSITIONS', function () {
 				done();
 			jchaos.sendCUCmd(cu_status, "start", "", null);
 
-			jchaos.checkLive(cu_status, 10, 1000, function (ds) { return (ds.health.nh_status == "Start"); }, function () { done(0); }, function () { done(1) });
+			jchaos.checkLive(cu_status, 10, 5000, function (ds) { return (ds.health.nh_status == "Start"); }, function () { done(0); }, function () { done(1) });
 
 		});
 	});
