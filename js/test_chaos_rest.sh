956d3985af4919ce3b93c2d4636c373673085069
amichelo <andrea.michelotti@lnf.infn.it>
2018-06-07 17:12:35 +0200

    dont test mds

--- js/test_chaos_rest.sh
+++ js/test_chaos_rest.sh
@@ -1,5 +1,6 @@
 source $CHAOS_TOOLS/common_util.sh
 start_test
+
 info_mesg "REST tests" " $0"
 if ! which node>&/dev/null;then
     if which nodejs >& /dev/null;then
@@ -11,11 +12,26 @@ if ! which node>&/dev/null;then
     ln -sf `which nodejs` node
     export PATH=$PATH:.
 fi
-export USNAME=UnitServer
-if ! check_proc mds ;then
-    nok_mesg "mds running"
-    end_test 1 "mds dead"
+start_services || end_test 1 "cannot start services"
+
+
+if [ -e "$CHAOS_TOOLS/../etc/localhost/MDSConfig.txt" ];then
+    MDS_TEST_CONF=$CHAOS_TOOLS/../etc/localhost/MDSConfig.txt
+    ok_mesg "found $MDS_TEST_CONF"
+else
+    nok_mesg "cannot find $CHAOS_TOOLS/etc/localhost/MDSConfig.txt"
+    end_test 1 "Cannot find MDS_TEST_CONF"
 fi
+   
+info_mesg "using configuration " "$CHAOS_TOOLS/etc/localhost/MDSConfig.txt"
+if run_proc "$CHAOS_PREFIX/bin/ChaosMDSCmd --mds-conf $MDS_TEST_CONF $CHAOS_OVERALL_OPT >& $CHAOS_PREFIX/log/ChaosMDSCmd.log;" "ChaosMDSCmd"; then
+    ok_mesg "Transfer test configuration \"$MDS_TEST_CONF\" to MDS"
+else
+    nok_mesg "Transfer test configuration \"$MDS_TEST_CONF\" to MDS"
+    end_test 1 "trasfering configuration"
+fi
+
+export USNAME=UnitServer
 echo "1512080677 1277.13 836.85 0.0 84800.0 1 106 FFFFFFFF FFFFFFFF FFFFFFFF FFC00000 106 FFFFFFFF FFFFFFFF FFFFFFFF FFC00000 3 2 2 210258 0 -1 5.33e+00 1.67e+02 0 368667000 0.000 0.000 5201.4 0.0 1 0 0.000 0.000 0.000 0.000 -3.550 -0.370 -2.940 -0.510 -1.720 -0.470 1.360 0.490 20.33 1770.00 18.90 20.89 1.842 1.782 -0.745 1.520 " > newdafne.stat
 
 if launch_us_cu 1 100 $CHAOS_MDS $USNAME TEST 1;then
